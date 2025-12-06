93ebe89020da8314916a7a252b0e46996eef8b45
Jake Bolton <ninmonkeys@gmail.com>
2025-12-06 15:37:16 -0600

    feat: `Get-NothingConfig` ( closes #2 )

---
+++ Commands/Get-NothingConfig.ps1
@@ -0,0 +1,26 @@
+﻿
+function Get-NothingConfig {
+    <#
+   .synopsis
+        Get Named Paths for EverythingSearch
+    .example
+        > Get-NothingConfig
+    #>
+    [Alias( 'Ns.Get-Config' )]
+    [OutputType( [System.IO.FileInfo] )]
+    [CmdletBinding()]
+    param(
+        # Includes Csv
+        [Alias('All')]
+        [switch] $ListAll
+    )
+    process {
+        Get-ChildItem -path ( join-path $Env:AppData 'Everything' )
+            | ?{
+                if( $ListAll ) { return $true }
+                $_.Name -eq 'Everything.Ini'
+            }
+            | Where-Object { $_.Extension -match '\.(ini|csv)' }
+            | Where-Object { $_.Name -notmatch 'old' }
+    }
+}
