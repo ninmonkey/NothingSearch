20c05523b7f23774a6209fc21535e03960862f47
Jake Bolton <ninmonkeys@gmail.com>
2025-12-10 14:10:51 -0600

    release: build `v0.0.3`

--- NothingSearch.psd1
+++ NothingSearch.psd1
@@ -8,7 +8,7 @@
     RootModule        = 'NothingSearch.psm1'
 
     # Version number of this module.
-    ModuleVersion     = '0.0.2'
+    ModuleVersion     = '0.0.3'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
--- NothingSearch.psm1
+++ NothingSearch.psm1
@@ -16,7 +16,10 @@ function Get-NothingConfig {
     .example
         # longest/full list of all file
         > Get-NothingConfig -All -IncludeBackups
-
+    .LINK
+        Get-NothingConfig
+    .LINK
+        Get-NothingConfigContent
     #>
     [Alias( 'Ns.Get-Config' )]
     [OutputType( [System.IO.FileInfo] )]
@@ -49,6 +52,91 @@ function Get-NothingConfig {
             }
             | Where-Object { $_.Extension -match '\.(ini|csv)' }
             | Where-Object { $_.Name -notmatch 'old' }
+    }
+}
+
+
+
+function Get-NothingConfigContent {
+    <#
+   .synopsis
+        Read config files by an alias, mapping to the real filepaths for you
+    .example
+    .LINK
+        Get-NothingConfig
+    .LINK
+        Get-NothingConfigContent
+    #>
+    [Alias( 'Ns.Get-ConfigContent' )]
+    [OutputType(
+        [PSCustomObject[]], # when: -not PassThru
+        [string[]] # when: PassThru
+    )]
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+            [Alias('Name', 'Alias')]
+            [ValidateSet(
+                'Everything.ini',
+                'Bookmarks',
+                'Filters',
+                'Macros',
+                'RunHistory',
+                'SearchHistory' )]
+            [string] $ConfigName,
+
+        # Return file as text, if automatic import does not work
+        [switch] $RawText
+    )
+    end {
+        $fullName = _Ns.Find-ConfigPathByName -ConfigName $ConfigName | Get-Item
+        if( -not $FullName ) { throw "Error resolving '${ConfigName}' to path!" }
+
+        if( $RawText ) { Get-Content -Raw $Fullname -Encoding Utf8 ; return; }
+        Import-Csv -Path $FullName -Encoding Utf8
+    }
+}
+
+
+function _Ns.Find-ConfigPathByName {
+    <#
+    .synopsis
+        maps names like 'Bookmarks' their absolute paths: "${Env:AppData}\Everything\Bookmarks-1.5a.csv"
+    .NOTES
+        Maybe this should be resolve/convert? but it's private.
+    .EXAMPLE
+        > _Ns.Find-ConfigPathByName Filters
+        # out: Join-Path $Env:AppData 'Everything\Bookmarks-1.5a.csv'
+    #>
+    param(
+        [Parameter(Mandatory)]
+            [Alias('Name', 'Alias')]
+            [ValidateSet(
+                'Everything.ini',
+                'Bookmarks',
+                'Filters',
+                'Macros',
+                'RunHistory',
+                'SearchHistory' )]
+            [string] $ConfigName
+    )
+    end {
+        $csv = Get-NothingConfig -ListCsv
+        # auto map names
+        foreach( $item in $csv ) {
+            $autoName = @( $item.Name -split '-', 2 )[0]
+            $autoName = $autoName -replace '\s*', ''
+            if( $autoName -match $ConfigName ) {
+                return $item
+            }
+        }
+        # otherwise fall back to mapping
+        switch( $ConfigName ) {
+            'Everything.ini' { Get-NothingConfig; return }
+            default {
+                throw "Unhandled Config was not found: '$ConfigName'"
+            }
+        }
     }
 }
 
